2cc74bf19b3889b7d96284b3025bd639ef09d6ea
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2025-11-22 13:42:25 +0530

    add nojekyll config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { writeFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Portfolio/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "create-nojekyll",
+      closeBundle() {
+        writeFileSync("dist/.nojekyll", "");
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
